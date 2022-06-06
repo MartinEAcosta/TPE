753ddafe0c78952c9c87925f2a9e9b1cd006b07e
Martin Acosta <acostamartin1280@gmail.com>
2022-06-06 12:05:56 -0300

    Proyecto inicial

---
+++ TPE/js/captcha.js
@@ -0,0 +1,20 @@
+let random = Math.random() * 100000;
+random = Math.floor (random) ;
+let numero = document.querySelector ("#NumRandom");
+numero.innerHTML=random;
+let boton = document.querySelector ("#botonCaptcha");
+let inputCaptcha = document.querySelector("#Ingresarcaptcha");
+let Resultado = document.querySelector ("#respuesta");
+
+boton.addEventListener("click",Send);
+
+function Send(){
+
+if(inputCaptcha.value == random){
+Resultado.innerHTML = "Su respuesta es correcta"
+}
+else{
+Resultado.innerHTML = "Su respuesta es incorrecta"
+}
+
+}

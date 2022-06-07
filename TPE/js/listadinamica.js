2287508b310dc7c57885036b46bd72a0dd6a076d
Martin Acosta <acostamartin1280@gmail.com>
2022-06-07 13:16:36 -0300

    + intento de tabla dinamica

--- TPE/js/listadinamica.js
+++ TPE/js/listadinamica.js
@@ -1,7 +1,26 @@
 "use strict"
 
-document.querySelector('#btn-incrementar').addEventListener('click', sumarprod);
+let btnagregar = document.querySelector("#btn-agregar");
+btnagregar.addEventListener('click', agregarprod);
 
-function sumarprod(){
-    console.log("funciona?");
+let btnquitar = document.querySelector("#btn-quitar");
+btnquitar.addEventListener('click', quitarprod);
+
+let compras = [];
+
+function agregarprod() {
+    console.log("Agregar producto al carrito");
+    let Producto = document.querySelector('#Producto').value;
+    let Precio = parseInt(documen.querySelector('#Precio').value);
+    let Cantidad = parseInt(document.querySelector('#Cantidad').value);
+    let renglon = {
+        "Producto": Producto,
+        "Precio": Precio,
+        "Cantidad": Cantidad,
+        "totalItem": Precio*Cantidad,
+    }
+    compras.push(renglon);
+    document.querySelector("#lista").innerHTML += "<li>" + renglon.Producto + " Cantidad " + renglon.Cantidad + "</li>";
+    documentquerySelector('#Producto').value = "";
 }
+function quitarprod(){}

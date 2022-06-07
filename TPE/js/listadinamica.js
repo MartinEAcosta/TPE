8fe010c8ad9dea143684c787ce4c3cd69b809b1b
Martin Acosta <acostamartin1280@gmail.com>
2022-06-07 12:23:25 -0300

    + botones + menu desplegable.js

---
+++ TPE/js/listadinamica.js
@@ -0,0 +1,7 @@
+"use strict"
+
+document.querySelector('#btn-incrementar').addEventListener('click', sumarprod);
+
+function sumarprod(){
+    console.log("funciona?");
+}
---
+++ TPE/js/menu.js
@@ -0,0 +1,7 @@
+"use strict"
+let marcostrolo = 1
+function {
+    for(marcostrolo<pos; marcostrolo++)
+    if(marcostrolo != 0 && marcostrolo== trolaso)
+        console.log("efectivamente, trolaso")
+}

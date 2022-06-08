73878a60b4ce80647f5d0e35c88ca58a066a6ff6
Martin Acosta <acostamartin1280@gmail.com>
2022-06-08 12:47:51 -0300

    + lista desplegable + media screen

--- TPE/js/listadinamica.js
+++ TPE/js/listadinamica.js
@@ -7,23 +7,29 @@ btnagregar.addEventListener('click', agregarprod);
 
 let btntotal = document.querySelector("#btn-total");
 btntotal.addEventListener('click', totalprod);
+ 
+let btnlimpiar = document.querySelector("#btn-limpiar");
+btnlimpiar.addEventListener('click', limpiarcarrito);
+
+let btnquitar = document.querySelector("#btn-quitar");
+btnquitar.addEventListener('click', quitaruno);
 
 let compras = [];
 
 function agregarprod() {
     console.log("Agregar producto al carrito");
     let Producto = document.querySelector('#Producto').value;
-    let Precio = parseInt(documen.querySelector('#Precio').value);
+    let Precio = parseInt(document.querySelector('#Precio').value);
     let Cantidad = parseInt(document.querySelector('#Cantidad').value);
     let renglon = {
         "Producto": Producto,
         "Precio": Precio,
         "Cantidad": Cantidad,
-        "totalItem": Precio*Cantidad,
+        "totalItem": Precio*Cantidad
     }
     compras.push(renglon);
     document.querySelector("#lista").innerHTML += "<li>" + renglon.Producto + " Cantidad " + renglon.Cantidad + "</li>";
-    documentquerySelector('#Producto').value = "";
+    document.querySelector('#Producto').value = "";
 }
 function totalprod(){
     console.log("Total de precio")
@@ -39,7 +45,18 @@ function totalprod(){
         if(max< r.totalItem)
             max = r.totalItem;
     }
-    documentquerySelector("#total").innerHTML = 
+    document.querySelector("#total").innerHTML = 
         "Total: $" + total +
         " Maximo: $" + max;
+}
+
+function limpiarcarrito(total) {
+    console.log("Limpiar carrito")
+    document.querySelector("#lista").innerHTML = "";
+}
+
+function quitaruno(renglon) {
+    console.log("Quitar ultimo producto")
+    compras.push(renglon); 
+    document.querySelector("#lista").innerHTML -= "<li>" + "</li>";
 }
--- TPE/js/menu.js
+++ TPE/js/menu.js
@@ -1,1 +1,6 @@
 "use strict"
+document.querySelector(".lista-btn").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    document.querySelector(".botones").classList.toggle("show");
+}
